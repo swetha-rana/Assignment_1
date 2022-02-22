df6dd04004a41f07a5c9a7f21864faaa3abf7da3
swetha <99182713+swetha-rana@users.noreply.github.com>
2022-02-22 11:52:17 +0530

    question-2.py
    This python script will download fashion-mnist data set and performs feed-forward neural network. fashion-mnist data as input and outputs a probability distribution over the 10 classes. 
    The probability distribution over the 10 clasess is stored in in a variable named "probability"

---
+++ question-2.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 22:06:21 2022
+
+@author: tenet
+"""
+
+
+from tensorflow.keras.datasets import fashion_mnist
+import numpy as np
+from numpy import exp
+
+(train_x_orig,train_y),(test_x_orig,test_y)= fashion_mnist.load_data()
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+train_y = train_y.reshape(1, len(train_y))
+test_y = test_y.reshape(1, len(test_y))
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+no_of_class=10
+
+train_x = train_x_flatten/255
+test_x = test_x_flatten/255
+layers_dims = [len(train_x),256,128,no_of_class]
+onehot_encoded = list()
+
+for i in range(train_y.shape[1]):
+    c=train_y[:,i][0]
+    letter = [0 for _ in range(no_of_class)]
+    letter[c] = 1
+    onehot_encoded.append(letter)
+
+Y=np.array(onehot_encoded)
+layers_dims = [len(train_x),256,128,no_of_class]
+
+# Function: initialize_parameters_ 
+# used for initialising the parameters of feed forward network
+# Returns parameters
+def initialize_parameters_(layer_dims):
+   
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)            # number of layers in the network
+
+    for l in range(1, L):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters['b' + str(l)] =  np.zeros((layer_dims[l], 1))
+        ### END CODE HERE ###
+       
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+       
+    return parameters
+
+# Function: feed_forward
+# peroforms the feed forward    
+#returns post activation(Z), pre activation (cache) values 
+def feed_forward(A, W, b):
+   
+    Z = np.dot(W, A) + b
+    cache = (A, W, b)
+    return Z, cache
+
+#function: sigmoid
+#performs the sigmoid activation function
+
+def sigmoid(Z):
+   
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    return A, cache
+
+#function: sigmoid
+#performs the ReLu activation function
+def relu(Z):    
+    A = np.maximum(0,Z)
+    cache = Z 
+    return A, cache
+
+#function: activation_forward
+#calls the feed forward and sigmoid or Relu activation functions
+#called from 
+def activation_forward(A_prev, W, b, activation):
+    if activation == "sigmoid":
+
+        Z, linear_cache  = feed_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+   
+    elif activation == "relu":
+        Z, linear_cache = feed_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
+#Function : L_model_forward
+# Will perform forward propagation 
+
+def L_model_forward(X, parameters):
+   
+    caches = []
+    A = X
+    L = len(parameters) // 2 
+   
+    for l in range(1, L):
+        A_prev = A
+        A, cache = activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], activation = "sigmoid")
+        caches.append(cache)
+          
+    AL, cache = activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation = "sigmoid")
+    caches.append(cache)
+
+    print(AL.shape)
+    return AL, caches
+
+#Function : L_layer_model
+#Will intialize parameters by calling initialize_parameters_
+#Calls L_model_forward to perform feed forward
+
+def L_layer_model(X, Y, layers_dims):
+   
+    parameters = initialize_parameters_(layers_dims)
+    AL, caches = L_model_forward(X, parameters)
+    return parameters,caches,AL
+
+parameters,caches,AL = L_layer_model(train_x, Y, layers_dims)
+Z=AL.reshape(60000,10)
+A3=caches[2][1].reshape(m_train,no_of_class)
+A2=caches[1][1].reshape(m_train,layers_dims[2])
+A1=caches[0][1].reshape(m_train,layers_dims[1])
+
+
+
+# function: softmax
+# perform softmax activation function to calculate the relative probabilities
+def softmax(n):
+    e = exp(n)
+    return e / e.sum()
+
+import numpy as np
+probability=np.empty(shape=(no_of_class,m_train))
+
+for i in range(0,m_train):
+        n=Z[i]
+        probability[:,i]=softmax(n)
+    
